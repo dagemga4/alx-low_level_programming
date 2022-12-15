097d21ecf2e50a107dc26c77273816c732992af8
dagemga4 <dagemga4@gmail.com>
2022-12-15 05:17:13 -0800

    msg

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,31 @@
+#include "main.h"                                                                                                                                               
+
+                                                                                                                                                                
+
+/**                                                                                                                                                             
+ *
+ *  * is_digit -                                                                                                                                                                                                                                                     
+ *
+ *   * Return: 1 0                                                                                                                                      
+ *
+ *    */                                                                                                                                                             
+
+                                                                                                                                                                
+
+int _isdigit(int c)                                                                                                                         
+
+{ 
+
+	if (c >= '0' && c<= '9'){
+
+		return 1;
+
+	}else{
+
+		return 0;
+
+	}
+
+}
+
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"                                                                                                                                               
+
+                                                                                                                                                                
+
+/**                                                                                                                                                             
+ *
+ *  * mul- multiply two numbers:                                                                                                                                                                                                                                                     
+ *
+ *   * Return: multiple                                                                                                                                    
+ *
+ *    */                                                                                                                                                             
+
+                                                                                                                                                                
+
+int mul(int a, int b)                                                                                                                      
+
+{ 
+
+	return a * b;
+
+}
+
+
