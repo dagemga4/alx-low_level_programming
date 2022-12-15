861806ab47dfafb8a348f554364783915c076fba
dagemga4 <dagemga4@gmail.com>
2022-12-15 04:19:12 -0800

    msg

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,30 @@
+#include "main.h"                                                                                                                                               
+
+                                                                                                                                                                
+
+/**                                                                                                                                                             
+ *
+ * is_digit -                                                                                                                                        
+ * Return: 1 0                                                                                                                                      
+ *
+ *
+ *
+ * */                                                                                                                                                             
+
+                                                                                                                                                                
+
+int _isupper(int c)                                                                                                                         
+
+{ 
+
+	if (c >= 'A' && c<= 'Z'){
+
+		return 1;
+
+	}else{
+
+		return 0;
+
+	}
+
+}
